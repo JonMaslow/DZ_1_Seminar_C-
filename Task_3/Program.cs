6c493f1f781425d71f5e34157a78c62de5eb1ce1
JonMaslow <114214959+JonMaslow@users.noreply.github.com>
2022-10-04 18:11:15 +0500

    DZ complick

---
+++ Task_3/Program.cs
@@ -0,0 +1,17 @@
+﻿/*Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным
+(делится ли оно на два без остатка).
+
+4 -> да
+-3 -> нет
+7 -> нет*/
+
+Console.WriteLine("Введите число:");
+int number = int.Parse(Console.ReadLine());
+int numA = number % 2;
+
+if (numA == 1)
+    Console.WriteLine($"{number} - является не чётным числом");
+if (numA == -1)
+    Console.WriteLine($"{number} - является не чётным числом");
+if (numA == 0)
+    Console.WriteLine($"{number} - является чётным числом");
